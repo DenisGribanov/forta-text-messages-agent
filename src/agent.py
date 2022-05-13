11e481d803ba20c199f18d79fe9549dde35da215
root <777>
2022-05-13 18:52:20 +0300

    commit

--- src/agent.py
+++ src/agent.py
@@ -1,60 +1,65 @@
-from forta_agent import Finding, FindingType, FindingSeverity, get_transaction_receipt, get_web3_provider
-import os
-import sys
+from forta_agent import Finding, FindingType, FindingSeverity, get_web3_provider
+import forta_agent
+
 
 EMPTY_DATA = '0x'
-ZERO_VALUE = 0
-
-def provide_handle_transaction(get_transaction_receipt):
-    def handle_transaction(transaction_event):
-        findings = []
-
-        receipt = get_transaction_receipt(transaction_event.hash)
-
-        # The contract was called. This doesn't fit
-        if len(receipt.logs) > 0 :
-            return findings
-
-        # To send a message, you need to send at least a little Ether
-        if receipt.transaction.value == ZERO_VALUE:
-            return findings
-
-        # empty
-        if receipt.transaction.data is None or receipt.transaction.data == EMPTY_DATA:
-            return findings
-
-        text_msg = tx_data_to_text(receipt.transaction.data)
-
-        if text_msg is None or text_msg == "":
-            return findings
+ZERO_VALUE = '0x0'
+words = ["you", "looser","scam", "lmao", "nitwit", "fuck","suck","fucking","cunt","bullshit",
+         "bitch","gay","ass","bastard","faggot","shit","stupid","asshole","virgin","penis","exploit","exploiter",
+         "exploitation","exploiter","exploiting","exploited","exploitative","exploitable","hacker","hack",
+         "hacked","hacker","hacking","cheated","cheating","cheat","whale","fishing","attack","attackable","attacking","attacker","attacked", ]
 
 
-        findings.append(Finding({
-            'name': 'A text message has been sent',
-            'description': 'A text message was detected inside the transaction',
-            'alert_id': 'forta-text-messages-agent',
-            'type': FindingType.Info,
-            'severity': get_severity(text_msg),
-            'metadata': {
-                'message': text_msg,
-                'from' : receipt.transaction.from_,
-                'to' : receipt.transaction.to
-            }
-        }))
+def handle_transaction(transaction_event: forta_agent.transaction_event.TransactionEvent):
+    findings = []
 
-
+    # The contract was called. This doesn't fit
+    if len(transaction_event.logs) > 0:
         return findings
 
-    return handle_transaction
+    # the amount of ether sent must be greater than 0
+    if transaction_event.transaction is not None and transaction_event.transaction.value == 0:
+        return findings
+
+    # if more, then it is a function call in contact. we need a regular transfer of ether
+    if transaction_event.traces is not None and len(transaction_event.traces) > 1:
+        return findings
+
+    # empty data
+    if transaction_event.transaction.data is None or transaction_event.transaction.data == EMPTY_DATA:
+        return findings
+
+    text_msg = tx_data_to_text(transaction_event.transaction.data)
+
+    if text_msg is None or text_msg == "":
+        return findings
+
+    findings.append(Finding({
+        'name': 'A text message has been sent',
+        'description': 'A text message was detected inside the transaction',
+        'alert_id': 'forta-text-messages-agent',
+        'type': FindingType.Info,
+        'severity': get_severity(text_msg),
+        'metadata': {
+            'message': text_msg,
+            'from': transaction_event.transaction.from_,
+            'to': transaction_event.transaction.to,
+            'tx_hash': transaction_event.hash,
+        }
+    }))
+
+    return findings
 
 
 def get_severity(text_msg):
-    word = WordCache()
 
-    if word.exists_assault_world(text_msg):
-        return FindingSeverity.High
-    else:
-        return FindingSeverity.Medium
+    for word in words:
+        if word in text_msg:
+            return FindingSeverity.High
+        else:
+            continue
+
+    return FindingSeverity.Medium
 
 
 def tx_data_to_text(data):
@@ -63,39 +68,3 @@ def tx_data_to_text(data):
         return web3_provider.toText(data)
     except:
         return None
-
-
-real_handle_transaction = provide_handle_transaction(get_transaction_receipt)
-
-
-def handle_transaction(transaction_event):
-    return real_handle_transaction(transaction_event)
-
-
-class WordCache(object):
-    words = []
-
-    def __new__(cls):
-        if not hasattr(cls, 'instance'):
-            cls.instance = super(WordCache, cls).__new__(cls)
-        return cls.instance
-
-    def exists_assault_world(self, text):
-        file_path = os.path.join(sys.path[0], 'assault_word.txt')
-
-        if len(self.words) == 0 and os.path.exists(file_path):
-            file = open(file_path, "r")
-            self.words = file.read().splitlines()
-
-        for word in self.words:
-            find = text.find(word.strip())
-
-            if find != -1:
-                return True
-            else:
-                continue
-
-        return  False
-
-
-wordCache = WordCache()
--- src/agent_test.py
+++ src/agent_test.py
@@ -1,12 +1,5 @@
-from unittest.mock import Mock
 from forta_agent import FindingSeverity, FindingType, create_transaction_event, Transaction, TransactionEvent
-from agent import provide_handle_transaction
-
-mock_get_transaction_receipt = Mock()
-handle_transaction = provide_handle_transaction(mock_get_transaction_receipt)
-
-
-
+from agent import handle_transaction
 #2018-01-15-T-04-30-14-930Z
 #https://etherscan.io/tx/0xce1cf623814118aae3e1caaac7bb409fcdd1e48f0794e11b86500260e13d6bb4
 MESSAGE_1 = '0x323031382d30312d31352d542d30342d33302d31342d3933305a'
@@ -23,62 +16,86 @@ NOT_EQUAL_TO_ZERO = "0x2bc6cb30ec2000"
 #https://etherscan.io/tx/0xfba4c700815a9fee055889b06ec00b1b3fb89ed3c4a33a3bba2e32711c757dc0
 CALL_CONTRACT_DATA ="0xe63d38ed000000000000000000000000000000000000000000000000000000000000004000000000000000000000000000000000000000000000000000000000000000800000000000000000000000000000000000000000000000000000000000000001000000000000000000000000e4c808592a4f60b09350c20151b5f17ee2437564000000000000000000000000000000000000000000000000000000000000000100000000000000000000000000000000000000000000000000470de4df820000"
 
-ADDRESS_TO= "0xd152f549545093347a162dce210e7293f1452150"
-ADDRESS_FROM = "0x0036491206cbf7abd6ee9610b15426cf1b4a6f34"
+ONE_ADDRESS = "0x1111111111111111111111111111111111111111"
+TWO_ADDRESS = "0x2222222222222222222222222222222222222222"
 
 class TestMessagesAgent:
 
     def test_returns_empty_findings_if_logs_not_empty(self):
-        mock_get_transaction_receipt.return_value = Mock(logs=[{}])
-        tx_event = create_transaction_event({})
+        tx_event = create_transaction_event({
+            'transaction': {
+                'from': ONE_ADDRESS,
+                'to': TWO_ADDRESS,
+                'hash': "0"
+            },
+            'receipt': {
+                'logs': [{
+
+                }]}})
 
         findings = handle_transaction(tx_event)
         print(findings)
         assert len(findings) == 0
 
     def test_returns_empty_findings_if_sent_zero_ether(self):
+        tx_event = create_transaction_event({
+            'transaction': {
+                'from': ONE_ADDRESS,
+                'to': TWO_ADDRESS,
+                'hash': "0",
+                'value' : ZERO_ETHER
+            },
+            'receipt': {
+                'logs': []}})
 
-        t = Transaction({'value' : ZERO_ETHER, 'hash': TX_HASH})
-
-
-        mock_get_transaction_receipt.return_value = Mock(logs=[], transaction = t)
-
-        tx_event = create_transaction_event({})
 
         findings = handle_transaction(tx_event)
         print(findings)
         assert len(findings) == 0
 
     def test_returns_empty_findings_if_sent_empty_data(self):
-        t = Transaction({'value': NOT_EQUAL_TO_ZERO, 'hash': TX_HASH})
-
-        mock_get_transaction_receipt.return_value = Mock(logs=[], transaction=t)
-
-        tx_event = create_transaction_event({})
+        tx_event = create_transaction_event({
+            'transaction': {
+                'from': ONE_ADDRESS,
+                'to': TWO_ADDRESS,
+                'hash': "0",
+                'value': NOT_EQUAL_TO_ZERO
+            },
+            'receipt': {
+                'logs': []}})
 
         findings = handle_transaction(tx_event)
         print(findings)
         assert len(findings) == 0
 
     def test_returns_empty_findings_if_not_utf_data(self):
-        t = Transaction({'value': NOT_EQUAL_TO_ZERO, 'hash': TX_HASH, 'data' : CALL_CONTRACT_DATA})
-
-        mock_get_transaction_receipt.return_value = Mock(logs=[], transaction=t)
-
-        tx_event = create_transaction_event({})
+        tx_event = create_transaction_event({
+            'transaction': {
+                'from': ONE_ADDRESS,
+                'to': TWO_ADDRESS,
+                'hash': "0",
+                'value': NOT_EQUAL_TO_ZERO,
+                'data' : CALL_CONTRACT_DATA
+            },
+            'receipt': {
+                'logs': []}})
 
         findings = handle_transaction(tx_event)
         print(findings)
         assert len(findings) == 0
 
     def test_returns_severity_medium_findings(self):
-        t = Transaction({'value': NOT_EQUAL_TO_ZERO,
-                         'hash': TX_HASH,
-                         'data' : MESSAGE_1})
 
-        mock_get_transaction_receipt.return_value = Mock(logs=[], transaction=t)
-
-        tx_event = create_transaction_event({})
+        tx_event = create_transaction_event({
+            'transaction': {
+                'from': ONE_ADDRESS,
+                'to': TWO_ADDRESS,
+                'hash': "0",
+                'value': NOT_EQUAL_TO_ZERO,
+                'data': MESSAGE_1
+            },
+            'receipt': {
+                'logs': []}})
 
         findings = handle_transaction(tx_event)
 
@@ -87,21 +104,19 @@ class TestMessagesAgent:
         assert finding.severity == FindingSeverity.Medium
 
     def test_returns_severity_high_findings(self):
-        t = Transaction({'value': NOT_EQUAL_TO_ZERO,
-                         'hash': TX_HASH,
-                         'data' : MESSAGE_2,
-                         'from' : ADDRESS_FROM,
-                         'to':  ADDRESS_TO
-                         })
-
-        mock_get_transaction_receipt.return_value = Mock(logs=[], transaction=t)
-
-        tx_event = create_transaction_event({})
+        tx_event = create_transaction_event({
+            'transaction': {
+                'from': ONE_ADDRESS,
+                'to': TWO_ADDRESS,
+                'hash': "0",
+                'value': NOT_EQUAL_TO_ZERO,
+                'data': MESSAGE_2
+            },
+            'receipt': {
+                'logs': []}})
 
         findings = handle_transaction(tx_event)
 
         assert len(findings) == 1
         finding = findings[0]
-        assert finding.metadata['to'] == ADDRESS_TO
-        assert finding.metadata['from'] == ADDRESS_FROM
         assert finding.severity == FindingSeverity.High
